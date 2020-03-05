078eadc75e8aa4909ad2a6e192541deca950bc98
carterschafer <60490199+carterschafer@users.noreply.github.com>
2020-03-05 15:42:21 -0800

    Upload new displayImage

--- displayImage/sketch.js
+++ displayImage/sketch.js
@@ -1,12 +1,16 @@
 let img;
+var imgWidth;
+var aspectRatio;
 
 function preload() {
   img = loadImage("Matterhorn.jpg")
+  aspectRatio = 1908 / 1146;
+  imgWidth = 600;
 }
 
 function setup() {
-  createCanvas(1908, 1146);
-  image(img,0,0)
+  createCanvas(600, 600);
+  image(img,0,0,imgWidth, imgWidth / aspectRatio);
 }
 
 function draw() {
